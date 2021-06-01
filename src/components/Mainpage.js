bb301063897e17eabede4e82caf30141d848cb98
Ali Ashraf <adaydreamer10@gmail.com>
2021-06-01 17:31:06 +0200

    image

--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,14 +1,29 @@
 import React, { useState, useEffect } from "react";
 
+
+
 const Mainpage = (props) => {
             const [data, setData] = useState();
             const [word, setWord] = useState();
             const [wordNumber, setWordNumber] = useState(1);
             const [clickNumber, setClickNumber] = useState(0);
+            const [image, setImage] = useState();
             useEffect(() => {
+                
                return getData();
 
             }, []);
+            useEffect(() => {
+               return getImage();
+            }, [word]);
+
+            // const unplashFetch = () => {
+            //     useEffect(()=>{
+            //         let url = fetch(`https://api.unsplash.com/search/photos?page=1&query=${word}&per_page=1&client_id=e1lPg8hu5eMKx3CbfYx9vO8N6mZ7kgjzq9pnAU5YHZI`).then((res)=>{
+            //             return res.json()
+            //         }).then((res) => { setImage(res.results[0].urls.small)})
+            //     }, [setWord])
+            // }
 
             const getData = () =>{
                 fetch(props.url + wordNumber)
@@ -18,10 +33,21 @@ const Mainpage = (props) => {
                 .then((result) => {
                     setData(result);
                     setWord(result.word);
+                    getImage();
+                    // let url = fetch(`https://api.unsplash.com/search/photos?page=1&query=${word}&per_page=1&client_id=e1lPg8hu5eMKx3CbfYx9vO8N6mZ7kgjzq9pnAU5YHZI`).then((res)=>{
+                    //     return res.json()
+                    // }).then((res) => { setImage(res.results[0].urls.small)})
+                    //https://api.unsplash.com/search/photos?page=1&query=office&per_page=1&client_id=e1lPg8hu5eMKx3CbfYx9vO8N6mZ7kgjzq9pnAU5YHZI
                     console.log(result);
                 });
             }
-
+            const getImage = async () => {
+                let url = await fetch(`https://api.unsplash.com/search/photos?page=1&query=${word}&per_page=1&client_id=e1lPg8hu5eMKx3CbfYx9vO8N6mZ7kgjzq9pnAU5YHZI`).then((res)=>{
+                        return res.json()
+                    }).then((res) => { 
+                        setImage(res.results[0].urls.small)
+                    })
+            }
             const changeLang = () => {
 
                 if(clickNumber === 0){
@@ -40,6 +66,7 @@ const Mainpage = (props) => {
             return (
                 <div onClick={changeLang}>
                 <p>{word}</p>
+                <img src={image} />
                 </div>
             );
     };
